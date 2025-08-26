ff68369426255d93d00f5e33302c484d1abb7d4e
Gabriel Nobrega <gnobrega.contato@gmail.com>
2025-08-26 19:55:03 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Optimizer } = require('@parcel/plugin');
+
+module.exports = new Optimizer({
+  name: 'html-google-tag-manager-compatibility',
+  async optimize({ bundle, contents, map }) {
+    if (bundle.type === 'html') {
+      let html = contents.toString();
+      
+      // Modifica type="module" para type="text/javascript" para compatibilidade com Google Tag Manager
+      const originalHtml = html;
+      html = html.replace(/type="module"/g, 'type="text/javascript"');
+      html = html.replace(/type='module'/g, "type='text/javascript'");
+      
+      console.log('🔧 Plugin Parcel: Script type modificado para compatibilidade com Google Tag Manager');
+      
+      return {
+        contents: html,
+        map
+      };
+    }
+    
+    return { contents, map };
+  }
+});
